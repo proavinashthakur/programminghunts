72c3d7e7373a1d6953a5453a55d547033c978e72
Gurnoor <gurnoor.ameotech1@gmail.com>
2020-06-22 12:32:24 +0530

    Debub mode disab;ed

--- programminghunts/settings.py
+++ programminghunts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#4_83uqa)#4fs@6%y#hm2247iw*#rn=wema+6-@j-9$ec7ks^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
